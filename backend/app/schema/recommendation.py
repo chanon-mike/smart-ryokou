d2166672ebbc581dea2f1f2324963828bd9a757f
tungvt123 <s1f102100029@iniad.org>
2023-09-24 03:57:45 +0900

    Change prompt to openai + extract json from openai response

--- backend/app/schema/recommendation.py
+++ backend/app/schema/recommendation.py
@@ -9,9 +9,22 @@ class BudgetType(Enum):
     LARGE = "large"
 
 
+BUDGET_TYPE_JA = {
+    BudgetType.SMALL: "少なめ",
+    BudgetType.MEDIUM: "普通",
+    BudgetType.LARGE: "多め",
+}
+
+
 class TripStyle(Enum):
     RELAXED = "relaxed"
     PACKED = "packed"
+
+
+TRIP_STYLE_JA = {
+    TripStyle.RELAXED: "ゆっくり",
+    TripStyle.PACKED: "忙しい",
+}
 
 
 class FreeFormatQuery(BaseModel):
@@ -20,12 +33,14 @@ class FreeFormatQuery(BaseModel):
 
 class StructuredQuery(BaseModel):
     """
-    Only place and how_long are required. Other properties are optional
+    Place and duration and people_num are required. Other properties are optional
     """
 
     place: str
     duration: int = 1
+    people_num: int = 1
     budget: BudgetType | None
     trip_style: TripStyle | None
     interests: list[str] | None
-    foods: list[str] | None
+    trip_type: str | None
+    remarks: list[str] | None
--- backend/app/use_case/recommendation.py
+++ backend/app/use_case/recommendation.py
@@ -1,7 +1,7 @@
 import openai
 
 from app.core.config import settings
-from app.schema.recommendation import StructuredQuery
+from app.schema.recommendation import BUDGET_TYPE_JA, TRIP_STYLE_JA, StructuredQuery
 
 # Get openai api key
 opeanai_api_key = settings.OPENAI_API_KEY
@@ -10,40 +10,91 @@ openai.api_key = opeanai_api_key
 openai.api_base = "https://api.openai.iniad.org/api/v1"
 
 
+def extract_json(openai_response: str) -> str:
+    """
+    Extract json from openai response
+    """
+    json_start_pos = openai_response.find("{")
+    json_end_pos = openai_response.rfind("}")
+    json = openai_response[json_start_pos : json_end_pos + 1]
+    return json
+
+
 def generate_recommendation_structured_format_query(query: StructuredQuery) -> str:
     place = query.place
     duration = query.duration
+    people_num = query.people_num
     budget = query.budget
     trip_style = query.trip_style
     interests = query.interests
-    foods = query.foods
+    trip_type = query.trip_type
+    remarks = query.remarks
 
-    prompt = f"Recommend itinerary in {place} for {duration} days. "
+    prompt = f"""
+#目的
+あなたには私の旅行プランナーとして、下記の要望と制約条件に従って入力された観光地における観光プランを作成してください。
+#場所
+{place}
+#期間
+{duration}日間
+#人数
+{people_num}人
+"""
 
     if budget:
-        prompt += f"The budget for this trip is {budget.value}. "
+        prompt += f"#予算\n {BUDGET_TYPE_JA[budget]} \n"
 
     if trip_style:
-        prompt += f"This trip pace is {trip_style.value}. "
+        prompt += f"#旅行ペース\n {TRIP_STYLE_JA[trip_style]} \n"
 
     if interests:
-        prompt += (
-            f"In this trip, I want to have activities with {','.join(interests)}. "
-        )
+        prompt += f"#興味\n {', '.join(interests)}\n"
 
-    if foods:
-        prompt += f"In this trip, I want to eat {','.join(foods)}."
+    if trip_type:
+        prompt += f"#旅行の種類\n{trip_type}\n"
+
+    if remarks:
+        prompt += f"#備考\n {', '.join(remarks)}"
+
+    prompt += """
+#出力形式
+出力は概要などがなくデータだけ返してください。場所の説明は適度な長さでお願いします。
+```json
+{
+  "recommendation": [
+    {
+        "date": "日1",
+        "activities": [
+            {
+            "place": "xxxx",
+            "description": "xxxx"
+            },
+            {
+            ...
+            }
+        ],
+    },
+    {
+        ...
+    }
+  ]
+}
+```
+"""
 
     print("Prompt: ", prompt)
 
     try:
-        response = openai.ChatCompletion.create(
+        openai_response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
+                {"role": "system", "content": "あなたは私の旅行プランナー"},
                 {"role": "user", "content": prompt},
             ],
         )
-        return response["choices"][0]["message"]["content"]
+        response_content = openai_response["choices"][0]["message"]["content"]
+        return extract_json(response_content)
+
     except Exception as e:
         print("OpenAI error: ", e)
         raise Exception("OpenAI error")
@@ -54,6 +105,7 @@ def generate_recommendation_free_format_query(query: str) -> str:
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
+                {"role": "system", "content": "あなたは私の旅行プランナー"},
                 {"role": "user", "content": query},
             ],
         )
