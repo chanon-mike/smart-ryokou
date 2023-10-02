968ade54806ae5bf8f8f72827ad61210a5861def
chanon-mike <zzmikezz8520@gmail.com>
2023-10-02 10:43:39 +0900

    fix: allowed all cors url and remove cors_origin var

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,13 +3,12 @@ from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 
 from app.api.routes import routers as api_router
-from app.core.config import settings
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.CORS_ORIGIN,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
