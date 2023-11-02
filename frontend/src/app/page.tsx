c8a57d5ce305d6d47ee86156f975f8da84934723
chanon-mike <zzmikezz8520@gmail.com>
2023-11-02 17:03:01 +0900

    feat: refactor recommendation content and fix render data

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,27 @@
-import RecommendationApp from '@/components/recommendation/RecommendationApp';
+import Prompt from '@/components/recommendation/prompt/Prompt';
+import { Container, Box, Typography } from '@mui/material';
+import createTranslation from 'next-translate/useTranslation';
 
 const Home = () => {
-  return <RecommendationApp />;
+  const { t } = createTranslation('common');
+
+  return (
+    <Container>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        height="100dvh"
+        maxWidth="100%"
+      >
+        <Typography variant="h1" component="h1">
+          {t('title')}
+        </Typography>
+        <Prompt />
+      </Box>
+    </Container>
+  );
 };
 
 export default Home;
--- frontend/src/app/session/page.tsx
+++ frontend/src/app/session/page.tsx
@@ -1,21 +1,23 @@
 'use client';
 
 import SessionClient from '@/client/service/session/implement';
-import { RecommendationContext } from '@/components/recommendation/RecommendationContext';
+import { RecommendationProvider } from '@/components/recommendation/RecommendationContext';
 import RecommendationResult from '@/components/recommendation/RecommendationResult';
-import { CircularProgress } from '@mui/material';
+import type Session from '@/service/database/session/model';
+import { Box, CircularProgress } from '@mui/material';
 import { useSearchParams, useRouter } from 'next/navigation';
-import { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const SessionResult = () => {
   const searchParams = useSearchParams();
   const router = useRouter();
   const [loading, setLoading] = useState<boolean>(false);
-  const { setRecommendations, setTripTitle } = useContext(RecommendationContext);
+  const [data, setData] = useState<Session | null>(null);
 
   useEffect(() => {
     const setup = async () => {
       setLoading(true);
+
       const sessionId = searchParams.get('id');
       if (!sessionId) {
         setLoading(false);
@@ -31,28 +33,46 @@ const SessionResult = () => {
         return;
       }
 
-      setTripTitle(data.tripTitle);
-      setRecommendations(data.recommendations);
+      setData(data);
       setLoading(false);
     };
 
     setup();
-  }, [router, searchParams, setRecommendations, setTripTitle]);
+  }, [router, searchParams]);
 
-  return loading ? (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh', // Ensure it takes the full height of the viewport
-      }}
-    >
-      <CircularProgress />
-    </div>
-  ) : (
-    <RecommendationResult />
-  );
+  const renderContent = () => {
+    if (loading || !data) {
+      return (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh', // Ensure it takes the full height of the viewport
+          }}
+        >
+          <CircularProgress />
+        </div>
+      );
+    }
+
+    return (
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        height="100dvh"
+        maxWidth="100%"
+      >
+        <RecommendationProvider rec={data.recommendations || []} title={data.tripTitle || ''}>
+          <RecommendationResult />
+        </RecommendationProvider>
+      </Box>
+    );
+  };
+
+  return renderContent();
 };
 
 export default SessionResult;
