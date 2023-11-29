148ea1a96653dc39a79544f0ffda7215d48fd4ad
YadanarLin <85671303+YadanarLin@users.noreply.github.com>
2023-11-29 19:53:38 +0900

    added autocomplete. city, pref and country are shown in the end

--- frontend/src/components/prompt/InputBar.tsx
+++ frontend/src/components/prompt/InputBar.tsx
@@ -1,25 +1,88 @@
 'use client';
-
-import type { Dispatch, SetStateAction } from 'react';
-import { InputAdornment, TextField } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { InputAdornment, TextField, Button } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import useTranslation from 'next-translate/useTranslation';
 
+import MapConfigs from '@/libs/MapConfigs';
+
+const { API_KEY } = MapConfigs();
+
 type Props = {
   placeInput: string;
-  setPlaceInput: Dispatch<SetStateAction<string>>;
+  setPlaceInput: React.Dispatch<React.SetStateAction<string>>;
   handleOpenModal: () => void;
 };
 
 const InputBar = ({ placeInput, setPlaceInput, handleOpenModal }: Props) => {
-  const { t } = useTranslation('home');
+    const { t } = useTranslation('home');
+    const [autocomplete, setAutocomplete] = useState<google.maps.places.Autocomplete | null>(null);
+  
+    // Function to format the address as per your requirements
+    const formatShortAddress = (place: google.maps.places.PlaceResult): string => {
+      const city = place.address_components.find((component) =>
+        component.types.includes('locality')
+      )?.long_name;
+  
+      const prefecture = place.address_components.find((component) =>
+        component.types.includes('administrative_area_level_1')
+      )?.long_name;
+  
+      const country = place.address_components.find((component) =>
+        component.types.includes('country')
+      )?.long_name;
+  
+      // Customize the address format as needed
+      return `${city}, ${prefecture}, ${country}`;
+    };
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&libraries=places`;
+    script.async = true;
+    script.onload = () => {
+      const input = document.getElementById('search') as HTMLInputElement;
+
+      const options = {
+        types: ['establishment'],
+      };
+
+      const autoCompleteInstance = new google.maps.places.Autocomplete(input, options);
+      setAutocomplete(autoCompleteInstance);
+
+      autoCompleteInstance.addListener('place_changed', () => {
+        const place = autoCompleteInstance.getPlace();
+
+        if (place.geometry && place.geometry.location) {
+          // Shorten the address (e.g., take only the first two lines)
+          const shortenedAddress = formatShortAddress(place);
+          setPlaceInput(shortenedAddress);
+        } else {
+          console.error('Error retrieving place details:', place);
+        }
+      });
+
+    };
+
+    document.head.appendChild(script);
+
+    return () => {
+      // Cleanup the script tag to avoid memory leaks
+      document.head.removeChild(script);
+    };
+  }, []);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPlaceInput(event.target.value);
   };
 
+  const handleSearchClick = () => {
+    // Trigger form submission when the search icon is clicked
+    handleOpenModal();
+  };
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    console.log('Form submitted!');
     handleOpenModal();
   };
 
@@ -35,14 +98,16 @@ const InputBar = ({ placeInput, setPlaceInput, handleOpenModal }: Props) => {
         fullWidth
         required
         id="search"
-        type="search"
+        type="text"
         placeholder={t('input-message')}
         value={placeInput}
         onChange={handleChange}
         InputProps={{
           endAdornment: (
             <InputAdornment position="end">
-              <SearchIcon />
+              <Button onClick={handleSearchClick} style={{ cursor: 'pointer' }}>
+                <SearchIcon />
+              </Button>
             </InputAdornment>
           ),
         }}
