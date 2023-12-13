c7a30ddc36afd6dab94824385af406e55c882025
chanon-mike <zzmikezz8520@gmail.com>
2023-12-13 22:45:46 +0900

    feat: featured section

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,4 @@
+import Featured from '@/components/recommendation/prompt/Featured';
 import Prompt from '@/components/recommendation/prompt/Prompt';
 import { Container, Box, Typography } from '@mui/material';
 import createTranslation from 'next-translate/useTranslation';
@@ -10,20 +11,24 @@ const Home = () => {
 
   return (
     <Container>
-      <Box
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        marginTop={15}
-        maxWidth="100%"
-      >
-        <Box sx={{ display: 'flex', flexDirection: 'row', minHeight: 120 }}>
-          <Typography variant="h1">Smart</Typography>
+      <Box display="flex" flexDirection="column" marginTop={15} maxWidth="100%">
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            minHeight: 120,
+          }}
+        >
+          <Typography variant="h1" sx={{ paddingBottom: 1.5 }}>
+            Smart
+          </Typography>
           <Typography
             variant="h1"
             sx={{
               paddingLeft: 2,
+              paddingBottom: 1.5,
               backgroundcolor: 'primary',
               backgroundImage: `linear-gradient(90deg, rgba(77,112,217,1) 10%, rgba(217,77,112,1) 100%)`,
               backgroundSize: '100%',
@@ -36,10 +41,11 @@ const Home = () => {
             {ct('travel')}
           </Typography>
         </Box>
-        <Typography variant="subtitle1" color="text.secondary">
+        <Typography variant="subtitle1" color="text.secondary" sx={{ alignSelf: 'center' }}>
           {ht('caption')}
         </Typography>
         <Prompt />
+        <Featured />
       </Box>
     </Container>
   );
