903c733c4be3dfefc893f81cc2caf979f54dc9ba
chanon-mike <zzmikezz8520@gmail.com>
2023-12-23 01:03:06 +0900

    feat: add client interface

--- frontend/src/client/Client.ts
+++ frontend/src/client/Client.ts
@@ -2,11 +2,13 @@ import type { ClientInterface } from './ClientInterface';
 import getResult from './api/get-result/implement';
 import getLocation from './api/get-new-location/implement';
 import { getRestaurant } from './api/get-restaurant/implement';
+import { getDistanceMatrix } from './api/get-distance-matrix/implement';
 
 const Client: ClientInterface = {
   getResult,
   getLocation,
   getRestaurant,
+  getDistanceMatrix,
 };
 
 export default Client;
