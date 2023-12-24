72bd664218d71931bd8d18dad7e2602d25347dac
PhatTD <phattd@fpt.com>
2023-12-24 18:19:57 +0900

    Add directions with color

--- frontend/src/components/recommendation/map/Map.tsx
+++ frontend/src/components/recommendation/map/Map.tsx
@@ -4,7 +4,7 @@ import LocationDetail from '@/components/recommendation/map/LocationDetail';
 import { GOOGLE_MAPS_API_KEY } from '@/libs/envValues';
 import { mapStyles } from '@/libs/mapStyles';
 import type { Location } from '@/types/recommendation';
-import { GoogleMap, MarkerF, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, MarkerF, Polyline, useJsApiLoader } from '@react-google-maps/api';
 import { useContext, useEffect, useMemo } from 'react';
 import { ActiveLocationContext } from '../ActiveLocationContext';
 import { RecommendationContext } from '../RecommendationContext';
@@ -46,6 +46,22 @@ const Map = () => {
     [session],
   );
 
+  const paths = useMemo(() => {
+    return session.recommendations.flatMap((rec, recIndex) => {
+      const arrows = [];
+
+      for (let i = 0; i + 1 < rec.locations.length; i++) {
+        arrows.push({
+          key: `${recIndex}-${i}`,
+          path: [rec.locations[i], rec.locations[i + 1]],
+          color: fixedColors[recIndex % fixedColors.length],
+        });
+      }
+
+      return arrows;
+    });
+  }, [session]);
+
   const handleMarkerClick = (location: Location) => {
     setActiveLocation(location);
     setMapCenter({ lat: location.lat, lng: location.lng });
@@ -72,9 +88,32 @@ const Map = () => {
             disableDefaultUI: isMobile,
           }}
         >
+          {paths.map(({ path, color, key }) => (
+            <Polyline
+              key={key}
+              path={path}
+              options={{
+                strokeColor: color,
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                icons: [
+                  {
+                    icon: {
+                      path: 'M 0,0 5,15 -5,15 0,0 z', // 0,0 is the tip of the arrow
+                      fillColor: color,
+                      fillOpacity: 1.0,
+                      strokeColor: color,
+                      strokeWeight: 1,
+                    },
+                    offset: '100%',
+                  },
+                ],
+              }}
+            />
+          ))}
           {allLocations.map((location) => (
             <MarkerF
-              key={`${location.id}-${location.color}}`}
+              key={`${location.id}-${location.name}-${location.color}}`}
               position={{ lat: location.lat, lng: location.lng }}
               onClick={() => handleMarkerClick(location)}
               icon={location.color}
