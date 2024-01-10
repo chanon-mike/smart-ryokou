de4a391c1b2f5fc951a0ee5dfd6dc3ddad4e5307
chanon-mike <zzmikezz8520@gmail.com>
2024-01-10 23:07:24 +0900

    feat: setup next-international

---
+++ frontend/locales/ja/result.ts
@@ -0,0 +1,34 @@
+export const result = {
+  result: {
+    title: '{{name}}の旅行プラン',
+    input: {
+      title: '新しい観光地を追加',
+      placeholder: 'Try AI: 博物館を探して',
+    },
+    'open-in-google-maps': 'Google Mapsで開く',
+    'get-directions': '行き先を調べる',
+    'get-walk-directions': '徒歩で行き先を調べる',
+    'get-drive-directions': '車で行き先を調べる',
+    'get-transit-directions': '公共交通機関で行き先を調べる',
+    deleteCardModal: {
+      title: 'この場所を削除しますか？',
+      ok: 'はい',
+      cancel: 'キャンセル',
+    },
+    'restaurant-title': 'レストランの結果',
+    'new-location': {
+      park: '公園',
+      'park-prompt': '公園探して',
+      aquarium: '水族館',
+      'aquarium-prompt': '水族館探して',
+      museum: '博物館',
+      'museum-prompt': '博物館探して',
+      shrine: '神社',
+      'shrine-prompt': '神社探して',
+      fun: '遊ぶところ',
+      'fun-prompt': '遊ぶところ探して',
+    },
+    'total-duration': '移動時間',
+    'display-routes': 'ルート表示',
+  },
+};
