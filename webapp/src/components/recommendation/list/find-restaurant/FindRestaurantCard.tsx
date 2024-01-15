9fd83ed8fc972ff8984597fb43e7fef78145dfb0
chanon-mike <zzmikezz8520@gmail.com>
2024-01-15 21:53:04 +0900

    feat: adjust restaurant card ui

--- webapp/src/components/recommendation/list/find-restaurant/FindRestaurantCard.tsx
+++ webapp/src/components/recommendation/list/find-restaurant/FindRestaurantCard.tsx
@@ -14,54 +14,67 @@ const FindRestaurantCard = ({ location, handleAddLocation }: FindRestaurantCardP
   return (
     <Card key={location.name} variant="elevation" sx={{ width: { sm: 1 / 6, xs: 4 / 6 } }}>
       <CardMedia component="img" height="100px" image={location.photo} alt={location.name} />
-      <CardContent>
-        <Typography gutterBottom variant="body1">
-          {location.name}
-        </Typography>
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            flexWrap: 'wrap',
-            gap: 0.25,
-            marginBottom: 1,
-          }}
-        >
-          <Typography variant="body2" color="text.secondary" sx={{ fontSize: '0.75rem' }}>
-            {location.rating}
+      <CardContent
+        sx={{
+          flexGrow: 1,
+          height: '100px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'end',
+        }}
+      >
+        <Box>
+          <Typography gutterBottom variant="body1">
+            {location.name}
           </Typography>
-          <Rating value={location.rating} precision={0.1} size="small" readOnly />
-          <Typography variant="body2" color="text.secondary" sx={{ fontSize: '0.75rem' }}>
-            ({location.userRatingCount})
-          </Typography>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              flexWrap: 'wrap',
+              gap: 0.25,
+              marginBottom: 1,
+            }}
+          >
+            <Typography variant="body2" color="text.secondary" sx={{ fontSize: '0.75rem' }}>
+              {location.rating}
+            </Typography>
+            <Rating value={location.rating} precision={0.1} size="small" readOnly />
+            <Typography variant="body2" color="text.secondary" sx={{ fontSize: '0.75rem' }}>
+              ({location.userRatingCount})
+            </Typography>
+          </Box>
+          {/* TODO: Make button stick to the bottom */}
         </Box>
+
         <Box
           sx={{
             display: 'flex',
             flexDirection: 'row',
             alignItems: 'center',
             justifyContent: 'space-between',
+            gap: 1,
           }}
         >
-          <Typography variant="body2" color="text.secondary" noWrap>
-            {location.description}
-          </Typography>
           <OpenInGoogleMapsButton
             placeId={location.placeId}
+            text="Google Maps"
             sx={{
-              p: 0.2,
+              px: 1,
+              py: 0.5,
               boxShadow: 0,
-              fontSize: '0.2rem',
-              justifyContent: 'space-evenly',
+              fontSize: '0.75rem',
+              justifyContent: 'start',
+              alignItems: 'center',
               overflow: 'hidden',
               textOverflow: 'ellipsis',
-              width: '15rem',
+              width: 'auto',
             }}
             color="primary"
             variant="contained"
           />
           <IconButton sx={{ padding: 0.5 }} onClick={() => handleAddLocation(location)}>
-            <AddCircleOutlineIcon color="primary" fontSize="small" />
+            <AddCircleOutlineIcon color="primary" />
           </IconButton>
         </Box>
       </CardContent>
